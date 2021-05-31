4bd081507b6079d27feb6b99fbccd3a7841996bf
alexey.nikolaenko <nashron.developer@gmail.com>
2021-05-31 22:00:01 +0300

    Framework с вспомогательными методами

---
+++ PlaygroundHelpers/PlaygroundHelpers/PlaygroundHelpers.h
@@ -0,0 +1,18 @@
+//
+//  PlaygroundHelpers.h
+//  PlaygroundHelpers
+//
+//  Created by Alexey Nikolaenko on 31.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlaygroundHelpers.
+FOUNDATION_EXPORT double PlaygroundHelpersVersionNumber;
+
+//! Project version string for PlaygroundHelpers.
+FOUNDATION_EXPORT const unsigned char PlaygroundHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaygroundHelpers/PublicHeader.h>
+
+
